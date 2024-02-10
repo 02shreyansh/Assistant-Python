c8176858e348280d9f33c4e730bf2d39b7db824e
02shreyansh <02.shreyansh.10@gmail.com>
2024-02-10 22:15:37 +0530

    new commit

--- jarvis.py
+++ jarvis.py
@@ -3,12 +3,13 @@ import speech_recognition as sr
 import datetime
 import os
 import cv2
+import pyjokes
 from  requests import get
 import wikipedia
 import webbrowser
 import pywhatkit as kit 
 import smtplib
-import sys
+
 
 engine= pyttsx3.init('sapi5')
 voices= engine.getProperty('voices')
@@ -48,7 +49,7 @@ def wish():
         speak("Good evening")
     speak("I am AI ,Please tell me how can i help you?")
 
-
+#send email
 def SendEmail(to,content):
     server=smtplib.SMTP('smtp.gmail.com',587)
     server.ehlo()
@@ -57,71 +58,106 @@ def SendEmail(to,content):
     server.sendmail('email id',to,content)
     server.close()
   
-
-if __name__== "__main__":
-    wish()
+#function to run
+def run_jarvis():
     
+    query = takecommand().lower()
+
+#logic buliding for task
+    # open notepad
+    if "open notepad" in query:
+        npath = "C:\\Windows\\system32\\notepad.exe"
+        os.startfile(npath)
+    #for hello and hi
+    elif "hello" in query or "yo" in query:
+        speak("hello,How are you")
     
-    while True:
-        query = takecommand().lower()
-
-        #logic buliding for task
-
-        if "open notepad" in query:
-            npath = "C:\\Windows\\system32\\notepad.exe"
-            os.startfile(npath)
+    #open cmd
         
-        elif "open command prompt" in query:
-            os.system("start cmd")
-        elif "open camera" in query:
-            cap=cv2.VideoCapture(0)
-            while True:
-                ret,img=cap.read()
-                cv2.imshow('webcam',img)
-                k=cv2.waitKey(50)
-                if k==27:
-                    break;
-            cap.release()
-            cv2.destroyAllWindows()
+    elif "open command prompt" in query:
+        os.system("start cmd")
+    
+    #open camera
+    elif "open camera" in query:
+        cap=cv2.VideoCapture(0)
+        while True:
+            ret,img=cap.read()
+            cv2.imshow('webcam',img)
+            k=cv2.waitKey(50)
+            if k==27:
+                break;
+        cap.release()
+        cv2.destroyAllWindows()
+    #ip address
         
-        elif "ip address" in query:
-            ip=get('https://api.ipify.org').text
-            speak(f"your IP address is {ip}")
-        elif "wikipedia" in query:
-            speak("searching Wikipedia")
-            query=query.replace("wikipedia" ,"")
-            results=wikipedia.summary(query,sentences=50)
-            speak("according to wikipedia")
-            speak(results)
-        elif "open youtube" in query:
-            url='https://www.youtube.com/'
-            webbrowser.open(url)
-        elif "open stack overflow" in query:
-            webbrowser.open("www.stackoverflow.com")
-        elif "open google" in query:
-            speak("what should i search on google")
-            cm=takecommand().lower()
-            webbrowser.open(f"{cm}")
+    elif "ip address" in query:
+        ip=get('https://api.ipify.org').text
+        speak(f"your IP address is {ip}")
 
-        elif "send message" in query:
-            kit.sendwhatmsg("+917982702178", "Hello!",2,24) # 2 24 means time
-        elif "play song on youtube" in query:
-            kit.playonyt("see you again")
-        elif "email to shreyansh" in query:
-            try:
-                speak("what should i say")
-                content =takecommand().lower()
-                to= "02.shreyansh.10@gmail.com"
-                SendEmail(to,content)
-                speak("Email has been sent")
-            except Exception as e:
-                print(e)
-                speak("sorry sir,I am not able to send email")
-        
-        elif "no thanks" in query:
-            speak("Thanks for using me sir,have a nice day sir")
-            sys.exit()
-        speak("sir,do you have any other work")
+    #wikipedia
+    elif "wikipedia" in query:
+        speak("searching Wikipedia")
+        query=query.replace("wikipedia" ,"")
+        results=wikipedia.summary(query,sentences=50)
+        speak("according to wikipedia")
+        speak(results)
+    
+    #open youtube
+
+    elif "open youtube" in query:
+        url='https://www.youtube.com/'
+        webbrowser.open(url)
+
+    #open stack Overflow
+    elif "open stack overflow" in query:
+        webbrowser.open("www.stackoverflow.com")
+    # open google
+    elif "open google" in query:
+        speak("what should i search on google")
+        cm=takecommand().lower()
+        webbrowser.open(f"{cm}")
+    # send whatsaap msg
+    elif "send message" in query:
+        kit.sendwhatmsg("+917982702178", "Hello!",2,24) # 2 24 means time
+    # play music
+    elif "play" in query:
+        song =query.replace("play","")
+        speak("playing" + song)
+        kit.playonyt(song)
+    # tell time
+    elif "time" in query:
+        time=datetime.datetime.now().strftime("%I:%M %p")
+        print(time)
+        speak("current time is " + time)
+    
+    # send email
+    elif "email to shreyansh" in query:
+        try:
+            speak("what should i say")
+            content =takecommand().lower()
+            to= "02.shreyansh.10@gmail.com"
+            SendEmail(to,content)
+            speak("Email has been sent")
+        except Exception as e:
+            print(e)
+            speak("sorry sir,I am not able to send email")
+    #tell joke
+    elif "joke" in query:
+        speak(pyjokes.get_joke())
+    # for exit    
+    elif "no thanks" in query or "exit" in query:
+        speak("Thanks for using me sir,have a nice day sir")
+        exit()
+
+    # when all statement false
+    else:
+        speak("i haven't understand,do you have any other work")
+
+# for wish        
+wish()  
+# to run continue 
+while True:
+    run_jarvis()
 
 
             
